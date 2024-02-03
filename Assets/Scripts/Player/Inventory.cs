d28dd42e3e64b22900c357696e3d36981eb21858
baedonggeun <hispanie3@gmail.com>
2024-02-03 20:52:52 +0900

    240203_inventory 및 items 설정
    1. Inventory 스크립트 작성 추가
    2. ItemSlotUI 스크립트 작성 추가
    3. player에 inventory 스크립트 추가 후 해당 변수에 맞는 오브젝트 달아줌
    4. 아이템들 prefab화
    5. scriptable objects의 data에 drop prefabs를 prefab화한 아이템들로 설정

--- Assets/Scripts/Player/Inventory.cs
+++ Assets/Scripts/Player/Inventory.cs
@@ -7,42 +7,42 @@ using UnityEngine.Events;
 using UnityEngine.InputSystem;
 using static UnityEditor.Progress;
 
-public class ItemSlot
+public class ItemSlot       //아이템 슬롯
 {
-    public ItemData item;
-    public int quantity;
+    public ItemData item;       //아이템
+    public int quantity;        //수량
 }
 
 public class Inventory : MonoBehaviour
 {
-    public ItemSlotUI[] uiSlots;
-    public ItemSlot[] slots;
+    public ItemSlotUI[] uiSlots;        //아이템 슬롯 키고 끄기 위한 UI 여러 개 배열로(개수 정해져 있기 때문)
+    public ItemSlot[] slots;        //아이템 슬롯
 
-    public GameObject inventoryWindow;
-    public Transform dropPosition;
+    public GameObject inventoryWindow;      //인벤토리 창 오브젝트
+    public Transform dropPosition;      //드랍 위치
 
     [Header("Selected Item")]
-    private ItemSlot selectedItem;
-    private int selectedItemIndex;
-    public TextMeshProUGUI selectedItemName;
-    public TextMeshProUGUI selectedItemDescription;
-    public TextMeshProUGUI selectedItemStatNames;
-    public TextMeshProUGUI selectedItemStatValues;
-    public GameObject useButton;
-    public GameObject equipButton;
-    public GameObject unEquipButton;
-    public GameObject dropButton;
+    private ItemSlot selectedItem;      //선택된 아이템 오브젝트
+    private int selectedItemIndex;      //선택된 아이템의 번호
+    public TextMeshProUGUI selectedItemName;        //선택된 아이템의 이름
+    public TextMeshProUGUI selectedItemDescription;     //선택된 아이템 설명
+    public TextMeshProUGUI selectedItemStatNames;       //선택된 아이템 스탯 이름
+    public TextMeshProUGUI selectedItemStatValues;      //선택된 아이템 스탯 값
+    public GameObject useButton;        //사용 버튼
+    public GameObject equipButton;      //장비 버튼
+    public GameObject unEquipButton;        //장비 해제 버튼
+    public GameObject dropButton;       //드랍 버튼
 
-    private int curEquipIndex;
+    private int curEquipIndex;      //현재 장비하고 있는 아이템의 번호
 
     private PlayerController controller;
     private PlayerConditions condition;
 
     [Header("Events")]
-    public UnityEvent onOpenInventory;
-    public UnityEvent onCloseInventory;
+    public UnityEvent onOpenInventory;      //인벤토리 창 활성화 이벤트
+    public UnityEvent onCloseInventory;     //인벤토리 창 비활성화 이벤트
 
-    public static Inventory instance;
+    public static Inventory instance;       //싱글톤
     void Awake()
     {
         instance = this;
@@ -51,10 +51,11 @@ public class Inventory : MonoBehaviour
     }
     private void Start()
     {
-        inventoryWindow.SetActive(false);
+        inventoryWindow.SetActive(false);       //인벤토리 메뉴 비활성화한 상태로 시작
         slots = new ItemSlot[uiSlots.Length];
+        //prefabs로 만들어 놓은 itemslot을 UI밑에 개수를 지정해서 넣었으므로 길이가 정해져 있음
 
-        for (int i = 0; i < slots.Length; i++)
+        for (int i = 0; i < slots.Length; i++)      //slots와 uiSlots 초기화
         {
             slots[i] = new ItemSlot();
             uiSlots[i].index = i;
@@ -73,70 +74,75 @@ public class Inventory : MonoBehaviour
     }
 
 
-    public void Toggle()
+    public void Toggle()        //인벤토리 창 키고 끄는 함수
     {
-        if (inventoryWindow.activeInHierarchy)
+        if (inventoryWindow.activeInHierarchy)      //하이라키 상에서 인벤토리 창이 켜져있으면 
         {
-            inventoryWindow.SetActive(false);
+            inventoryWindow.SetActive(false);       //인벤토리 창 끔
             onCloseInventory?.Invoke();
             //controller.ToggleCursor(false);
         }
-        else
+        else        //하이라키 상에서 인벤토리 창이 꺼져있으면
         {
-            inventoryWindow.SetActive(true);
+            inventoryWindow.SetActive(true);        //인벤토리 창 킴
             onOpenInventory?.Invoke();
             //controller.ToggleCursor(true);
         }
     }
 
-    public bool IsOpen()
+    public bool IsOpen()        //인벤토리 창이 켜져있는지 꺼져있는지 확인하는 함수
     {
-        return inventoryWindow.activeInHierarchy;
+        return inventoryWindow.activeInHierarchy;       //active값(true / false)를 반환
     }
 
-    public void AddItem(ItemData item)
+    public void AddItem(ItemData item)      //아이템 추가 함수
     {
-        if (item.canStack)
+        if (item.canStack)      //아이템이 스택 가능하다면(중첩)
         {
-            ItemSlot slotToStackTo = GetItemStack(item);
-            if (slotToStackTo != null)
+            ItemSlot slotToStackTo = GetItemStack(item);        
+            //추가하려는 아이템이 기존에 중첩된 아이템이 있는지 확인하기 위해 
+            //가져온 아이템의 중첩값을 새로운 아이템 슬롯에 넣음???
+            if (slotToStackTo != null)      //중첩된 아이템이 있다면
             {
-                slotToStackTo.quantity++;
-                UpdateUI();
+                slotToStackTo.quantity++;       //수량 추가
+                UpdateUI();     //UI 업데이트
                 return;
             }
         }
 
-        ItemSlot emptySlot = GetEmptySlot();
+        ItemSlot emptySlot = GetEmptySlot();        
+        //위 코드에서 return이 안 됐다면 스택가능 중첩아이템x or 스택 불가능인 아이템이므로 새로운 슬롯에 값 추가
 
-        if (emptySlot != null)
+        if (emptySlot != null)      //빈 슬롯이 있다면
         {
-            emptySlot.item = item;
-            emptySlot.quantity = 1;
-            UpdateUI();
+            emptySlot.item = item;      //빈 슬롯에 아이템 넣어줌
+            emptySlot.quantity = 1;     //수량 1개로 설정
+            UpdateUI();     //UI 업데이트
             return;
         }
 
         ThrowItem(item);
+        //위 코드에서 return이 안 됐다면 빈 슬롯도 없는 것이므로 드랍
     }
 
-    void ThrowItem(ItemData item)
+    void ThrowItem(ItemData item)       //아이템 드랍 함수
     {
         Instantiate(item.dropPrefab, dropPosition.position, Quaternion.Euler(Vector3.one * Random.value * 360f));
+        //랜덤한 회전을 갖고 지정한 위치에 매개변수로 들어온 ItemData 생성
     }
 
-    void UpdateUI()
+    void UpdateUI()     //슬롯의 UI 데이터 최신화
     {
         for (int i = 0; i < slots.Length; i++)
         {
-            if (slots[i].item != null)
-                uiSlots[i].Set(slots[i]);
+            if (slots[i].item != null)      //슬롯 배열의 해당 요소에 아이템이 있다면
+                uiSlots[i].Set(slots[i]);       //슬롯에 있는 데이터로 uiSlots의 데이터 최신화
             else
-                uiSlots[i].Clear();
+                uiSlots[i].Clear();     //아이템이 없다면 ui초기화
         }
     }
 
-    ItemSlot GetItemStack(ItemData item)
+    ItemSlot GetItemStack(ItemData item)        //아이템 중첩 개수 설정(최대값을 넘지 못하게) 함수
     {
         for (int i = 0; i < slots.Length; i++)
         {
@@ -199,7 +205,7 @@ public class Inventory : MonoBehaviour
         dropButton.SetActive(false);
     }
 
-    public void OnUseButton()
+    public void OnUseButton()       //아이템 사용 버튼 동작 시 작동해야할 함수
     {
         if (selectedItem.item.type == ItemType.Consumable)
         {
@@ -217,28 +223,33 @@ public class Inventory : MonoBehaviour
         RemoveSelectedItem();
     }
 
-    public void OnEquipButton()
+    public void OnEquipButton()     //장착 버튼 동작 시 작동해야할 함수
     {
 
     }
 
-    void UnEquip(int index)
+    void UnEquip(int index)     //장착 해제 함수
     {
 
     }
 
-    public void OnUnEquipButton()
+    public void OnUnEquipButton()       //장착 해제 버튼 동작 시 작동해야할 함수
     {
 
     }
-
-    public void OnDropButton()
+    /*
+     *아이템 드랍 버튼
+     *TrowItem 함수가 있지만 해당 함수는 드랍 기능만을 나타내고
+     *OnDropButton에서 드랍기능, 인벤토리에서 제거 등
+     * 드랍 시 작동할 함수 및 기능들을 버튼에 달아주기 위한 함수
+     */
+    public void OnDropButton()      //드랍 버튼 동작 시 작동해야할 함수
     {
-        ThrowItem(selectedItem.item);
-        RemoveSelectedItem();
+        ThrowItem(selectedItem.item);       //선택된 아이템 드랍
+        RemoveSelectedItem();       //인벤토리에서 해당 아이템 제거
     }
 
-    private void RemoveSelectedItem()
+    private void RemoveSelectedItem()       //인벤토리에서 선택된 아이템 제거 함수
     {
         selectedItem.quantity--;
 
@@ -256,7 +267,7 @@ public class Inventory : MonoBehaviour
         UpdateUI();
     }
 
-    public void RemoveItem(ItemData item)
+    public void RemoveItem(ItemData item)       //아이템 제거 함수
     {
 
     }
--- Assets/Scripts/UI/ItemSlotUI.cs
+++ Assets/Scripts/UI/ItemSlotUI.cs
@@ -10,7 +10,7 @@ public class ItemSlotUI : MonoBehaviour
     public Image icon;
     public TextMeshProUGUI quatityText;
     private ItemSlot curSlot;
-    private Outline outline;
+    private Outline outline;        //아이템 외곽 선(선택 시 활성화되며 아이템이 선택됐다는 표시를 함)
 
     public int index;
     public bool equipped;
@@ -20,32 +20,33 @@ public class ItemSlotUI : MonoBehaviour
         outline = GetComponent<Outline>();
     }
 
-    private void OnEnable()
+    private void OnEnable()     //equipped 값이 true면 아웃라인을 켜줌
     {
         outline.enabled = equipped;
     }
 
-    public void Set(ItemSlot slot)
+    public void Set(ItemSlot slot)      //아이템 슬롯을 전달해주면 그 슬롯으로 값을 세팅
     {
-        curSlot = slot;
-        icon.gameObject.SetActive(true);
-        icon.sprite = slot.item.icon;
+        curSlot = slot;     //현재 슬롯에 가져온 슬롯 입력
+        icon.gameObject.SetActive(true);        //아이콘 활성화
+        icon.sprite = slot.item.icon;       //아이콘 스프라이트를 가져온 슬롯의 아이템 아이콘으로 설정
         quatityText.text = slot.quantity > 1 ? slot.quantity.ToString() : string.Empty;
+        //슬롯의 수량 텍스트가 가져온 슬롯의 수가 1보다 큰 경우 그 숫자를 1보다 작거나 같은 경우 Empty로 설정
 
-        if (outline != null)
+        if (outline != null)        //아웃라인이 존재한다면 equipped 값을 넣고 아웃라인을 켜줌
         {
             outline.enabled = equipped;
         }
     }
 
-    public void Clear()
+    public void Clear()     //아이템 슬롯 제거
     {
-        curSlot = null;
-        icon.gameObject.SetActive(false);
-        quatityText.text = string.Empty;
+        curSlot = null;     //현재 아이템 슬롯 비우기
+        icon.gameObject.SetActive(false);       //아이콘 비활성화
+        quatityText.text = string.Empty;        //수량 텍스트 empty로 설정
     }
 
-    public void OnButtonClick()
+    public void OnButtonClick()     //슬롯 클릭시 인벤토리 클래스의 SelectItem 함수에 index를 넘겨주고 실행
     {
         Inventory.instance.SelectItem(index);
     }
